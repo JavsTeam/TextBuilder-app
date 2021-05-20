fdf897c5751451a395e81593e408f77e318c5084
V-Senex <junior.07.inator@gmail.com>
2021-05-20 23:48:00 +0300

    Init basic structure

---
+++ app/src/main/java/com/example/textbuilder/ui/ReadySourceFragment.kt
@@ -0,0 +1,8 @@
+package com.example.textbuilder.ui
+
+import androidx.fragment.app.Fragment
+import com.example.textbuilder.R
+
+class ReadySourceFragment : Fragment(R.layout.fragment_ready_source) {
+
+}
