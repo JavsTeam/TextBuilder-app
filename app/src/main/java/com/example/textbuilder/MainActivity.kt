32247d61a52d07f20f20d0a5190e314634267a66
V-Senex <junior.07.inator@gmail.com>
2021-05-22 13:54:31 +0300

    Dark theme issue solve

--- app/src/main/java/com/example/textbuilder/MainActivity.kt
+++ app/src/main/java/com/example/textbuilder/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.textbuilder
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.card_layout)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
     }
 }
